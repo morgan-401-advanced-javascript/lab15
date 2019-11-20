545a0b9b7c4d3d2425bb8d6876f532f0f9d21506
morgantatums <morgantatums@zillowgroup.com>
2019-11-19 17:04:46 -0800

    added uml

--- README.md
+++ README.md
@@ -15,4 +15,4 @@
 npm test
 
 #### UML
-Link to an image of the UML for your application and response to events
+![UML](./assets/UML.jpg)
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const fs = require('fs');
 const fileChanger = require('./src/file-changer.js');
+const  emitter = require('./src/emitter.js');
+const logger = require('./src/logger');
 
 let file = process.argv.slice(2).shift();
 
 fileChanger.alterFile(file);
+
 
